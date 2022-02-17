db1ad5f3eedcd67099017fede118806f95a4b6fc
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-02-17 09:54:49 -0500

    add lab12-5

---
+++ lab_12-5.py
@@ -0,0 +1,5 @@
+# author: LM (AMDG) 2/17/22
+row = {'B202-15': 'Rogers, Sean', 'B202-14': "Mendizabal, Luis", 'B202-13': 'Lugo, Ryan', 'B202-12': 'Iaropoli, James', 'B202-11': 'Iacono, Jack'}
+print(row)
+row['B202-14'] = 'someone else'
+print(row)
