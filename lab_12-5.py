f3fe59c588ffc6cac85241df33e9a500dffa70f0
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-02-23 10:10:53 -0500

    add lab12-5

--- lab_12-5.py
+++ lab_12-5.py
@@ -1,5 +1,7 @@
 # author: LM (AMDG) 2/17/22
+#dictioonary of computer and row
 row = {'B202-15': 'Rogers, Sean', 'B202-14': "Mendizabal, Luis", 'B202-13': 'Lugo, Ryan', 'B202-12': 'Iaropoli, James', 'B202-11': 'Iacono, Jack'}
 print(row)
+#changes person at this computer
 row['B202-14'] = 'someone else'
 print(row)
